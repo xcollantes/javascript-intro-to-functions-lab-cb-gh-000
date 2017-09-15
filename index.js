1bced3457b13ef67221869854b99639a5736b35d
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-15 23:51:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function shout (phrase)
   return phrase.toUpperCase();
   //phrase === phrase.toUpperCase() ? true : false;
 }
+
+function whisper (phrase)
+{
+  return phrase.toLowerCase();
+  //phrase === phrase.toUpperCase() ? true : false;
+}
